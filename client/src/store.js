831209d1ef0c57ef74f1fdd92a3ffb410311e48b
Cleberson F R Junior <cleberson.dev@gmail.com>
2018-12-12 11:05:55 -0300

    Removido middleware que permite debuggar a app com react pela extensão no navegador

--- client/src/store.js
+++ client/src/store.js
@@ -7,8 +7,7 @@ import rootReducer from './reducers';
 const middlewares = [thunk];
 
 const store = createStore(rootReducer, compose(
-  applyMiddleware(...middlewares),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(...middlewares)
 ));
 
 
